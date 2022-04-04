2c0e906a3369ac73fcbf1ff9bf6991e680a4858d
Phil Pepin <ppepin34@gmail.com>
2022-04-03 22:22:21 -0400

    add interval to update tasks automatically

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,23 +10,30 @@ var updateDay = function () {
 var updateTime = function () {
     // pull ids from html
     var times = $("li p").toArray();
+
+    //gets currentHour in military time
     var currentHour = currentTime.format("H");
     currentHour = parseInt(currentHour);
 
-    // run for loop to check against currentTime
+    // run for loop to check hour against ID and update classes
     for (var i = 0; i < times.length; i++) {
         // get id from array
         var id = parseInt(times[i].id);
 
-        //check id against time
+        //check for past
         if (currentHour < id) {
             $(times[i]).removeClass("present past")
             $(times[i]).addClass("future");
         }
+
+        // check for present
         else if (currentHour === id) {
             $(times[i]).removeClass("future past")
             $(times[i]).addClass("present");
-        } else if (currentHour > id) {
+        }
+
+        // check for future
+        else if (currentHour > id) {
             $(times[i]).removeClass("present future")
             $(times[i]).addClass("past");
         };
@@ -35,3 +42,8 @@ var updateTime = function () {
 
 updateDay();
 updateTime();
+
+// interval to update
+setInterval(function(){
+    updateTime();
+}, 300000)
